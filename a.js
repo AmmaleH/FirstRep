1f96d12a2bf515fc35cfb99f6df19f74d55626be
AmmaleH <2497859637@qq.com>
2021-08-28 21:44:35 +0800

    revert and commit(solved conflict)

--- a.js
+++ a.js
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 var a=3；
 const b = 4;
 let obj1 = { "a1":1, "b1":2  };
 var c = 21;  // 最新
+=======
+var a=
+>>>>>>> parent of a01f7ca (add various b)
 =======
 var a=
 >>>>>>> parent of a01f7ca (add various b)
